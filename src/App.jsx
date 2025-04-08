d167a8090de0f4bf55c2ba441d48df4dba4181b2
alenachankunju <alenachankunju@gmail.com>
2025-04-08 10:58:51 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import Counter from "./component/Counter";
+import CounterClass from "./component/CounterClass";
 
 function App() {
   return (
     <>
-      <Counter />
+      <CounterClass />
     </>
   );
 }
